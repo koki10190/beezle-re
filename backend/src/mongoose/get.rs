314e37d74595e69004037986d74ef016a96c465b
koki10190 <lukaodzelashvili@gmail.com>
2024-01-30 01:50:51 +0000

    Messaged!

--- backend/src/mongoose/get.rs
+++ backend/src/mongoose/get.rs
@@ -13,10 +13,7 @@ pub async fn get_document(
 
     let result = coll.find_one(Some(filter), None).await;
     match result {
-        Ok(doc) => {
-            println!("Found doc");
-            doc
-        }
+        Ok(doc) => doc,
         Ok(None) => None,
         Err(_) => None,
     }
