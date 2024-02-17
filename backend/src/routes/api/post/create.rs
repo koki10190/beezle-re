b9c55b040914b216189a0062c72d1b994e36223a
koki10190 <lukaodzelashvili@gmail.com>
2024-02-17 20:32:44 +0000

    Live reply notif update

--- backend/src/routes/api/post/create.rs
+++ backend/src/routes/api/post/create.rs
@@ -9,6 +9,7 @@ use crate::{
     beezle,
     mongoose::{self, add_coins, add_xp},
     poison::LockResultExt,
+    ws::{lib::send::send_back, structures::ws_data::WsData},
 };
 
 #[derive(Deserialize)]
@@ -23,6 +24,7 @@ struct TokenInfo {
 pub async fn route(
     body: web::Json<TokenInfo>,
     client: web::Data<mongodb::Client>,
+    fapp: web::Data<std::sync::Mutex<crate::data_struct::AppData>>,
 ) -> impl Responder {
     let token = decode::<mongoose::structures::user::JwtUser>(
         &body.token,
@@ -65,9 +67,24 @@ pub async fn route(
                     return HttpResponse::Ok().json(doc! {"error": "Invalid reply!"});
                 }
 
+                let mut app_data = fapp.lock().unwrap();
                 let unwrapped = replying_to_post.unwrap();
-
                 let handle = unwrapped.get("handle").unwrap().as_str().unwrap();
+
+                if let Some(receiver_session) = app_data.connections.get_mut(handle) {
+                    beezle::print("Found receiver");
+                    let data = WsData {
+                        channel: "get-notif".to_string(),
+                        json_data: serde_json::to_string(&doc! {
+                            "caller": &data.claims.handle,
+                            "post_id": &__post_id,
+                            "message": "replied to your post!"
+                        })
+                        .unwrap(),
+                    };
+                    send_back(receiver_session, data).await;
+                }
+
                 mongoose::update_document(
                     &client,
                     "beezle",
