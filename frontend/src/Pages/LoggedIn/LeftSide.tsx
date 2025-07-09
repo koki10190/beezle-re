35d945f87b0aad54ed867b4a8d9e547e2508684a
koki10190 <lukaodzelashvili@gmail.com>
2025-07-09 21:48:45 +0100

    Changed version

--- frontend/src/Pages/LoggedIn/LeftSide.tsx
+++ frontend/src/Pages/LoggedIn/LeftSide.tsx
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import { FormEvent, useEffect, useRef, useState } from 'react';
-import { BrowserRouter, Routes, Route, redirect } from 'react-router-dom';
-import { api_uri, discord, github, twitter, youtube } from '../../links';
-import { checkToken } from '../../functions/checkToken';
-import FollowBox from '../../Components/FollowBox';
-import { UserPrivate } from '../../types/User';
-import { fetchUserPrivate } from '../../functions/fetchUserPrivate';
+import axios from "axios";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { BrowserRouter, Routes, Route, redirect } from "react-router-dom";
+import { api_uri, discord, github, twitter, youtube } from "../../links";
+import { checkToken } from "../../functions/checkToken";
+import FollowBox from "../../Components/FollowBox";
+import { UserPrivate } from "../../types/User";
+import { fetchUserPrivate } from "../../functions/fetchUserPrivate";
 
 function LeftSide() {
     const [self_user, setSelfUser] = useState<UserPrivate | null>();
@@ -20,26 +20,26 @@ function LeftSide() {
         <div className="page-sides side-left">
             <h1>
                 Beezle
-                <span style={{ fontSize: '35px' }} className="re-text">
+                <span style={{ fontSize: "35px" }} className="re-text">
                     :RE
                 </span>
             </h1>
-            <h2 style={{ marginTop: '-20px', color: 'rgba(255,255,255,0.2)' }}>Alpha v1.0</h2>
+            <h2 style={{ marginTop: "-20px", color: "rgba(255,255,255,0.2)" }}>Alpha v1.1</h2>
             <div className="left-side-media">
-                <button onClick={() => window.open(github, '_blank')?.focus()} className="button-field button-field-grayblack">
+                <button onClick={() => window.open(github, "_blank")?.focus()} className="button-field button-field-grayblack">
                     <i className="fa-brands fa-github-alt"></i>
                 </button>
                 <button
-                    style={{ width: '65px' }}
-                    onClick={() => window.open(discord, '_blank')?.focus()}
+                    style={{ width: "65px" }}
+                    onClick={() => window.open(discord, "_blank")?.focus()}
                     className="button-field button-field-blurple"
                 >
                     <i className="fa-brands fa-discord"></i>
                 </button>
-                <button style={{ width: '57px' }} onClick={() => window.open(twitter, '_blank')?.focus()} className="button-field button-field-blue">
+                <button style={{ width: "57px" }} onClick={() => window.open(twitter, "_blank")?.focus()} className="button-field button-field-blue">
                     <i className="fa-brands fa-twitter"></i>
                 </button>
-                <button style={{ width: '60px' }} onClick={() => window.open(youtube, '_blank')?.focus()} className="button-field button-field-red">
+                <button style={{ width: "60px" }} onClick={() => window.open(youtube, "_blank")?.focus()} className="button-field button-field-red">
                     <i className="fa-brands fa-youtube"></i>
                 </button>
             </div>
@@ -51,7 +51,7 @@ function LeftSide() {
                         <FollowBox self_user={self_user} handle="beezle" />
                     </>
                 ) : (
-                    ''
+                    ""
                 )}
             </div>
         </div>
