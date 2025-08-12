dd980542b87d1525445f26a7e05c74206991f07d
koki10190 <lukaodzelashvili@gmail.com>
2025-08-12 16:47:56 +0100

    reverted

--- backend/src/routes/ws/socket.rs
+++ backend/src/routes/ws/socket.rs
@@ -17,68 +17,106 @@ pub async fn main_ws(
     body: web::Payload, 
     ws_sessions: web::Data<Arc<Mutex<HashMap<String, actix_ws::Session>>>>
 ) -> actix_web::Result<impl Responder> {
-    let (res, mut session, mut stream) = actix_ws::handle(&req, body)?;
+    let (res, session, msg_stream) = actix_ws::handle(&req, body)?;
 
-    actix_web::rt::spawn(async move {
-        let mut user_handle = String::new();
-        while let Some(message) = stream.next().await {
-            match message {
-                Ok(Message::Binary(_)) => {}
-                Ok(Message::Ping(_)) => {}
-                Ok(Message::Continuation(_)) => {}
-                Ok(Message::Pong(_)) => {}
-                Ok(Message::Close(_)) => {}
-                Ok(Message::Nop) => {}
-                Ok(Message::Text(msg)) => {
-                    let json_data: Result<MessageData,  serde_json::Error> = serde_json::from_str(&msg.to_string());
-                    println!("[WS] Got Message: {}", msg);
+    // spawn websocket handler (and don't await it) so that the response is returned immediately
+    spawn_local(ws(
+        session,
+        msg_stream,
+        ws_sessions
+    ));
 
-                    if let Err(data) = json_data {
-                        beezle::print(format!("WS Error: {:?}", data).as_str());
-                        break;
+    Ok(res)
+}
+
+
+async fn ws(mut session: Session, mut msg_stream: MessageStream, ws_sessions: web::Data<Arc<Mutex<HashMap<String, actix_ws::Session>>>>) {
+    let mut user_handle = String::new();
+
+    let mut last_heartbeat = Instant::now();
+    let mut interval = time::interval(HEARTBEAT_INTERVAL);
+    beezle::print("uh");
+
+    let _close_reason = loop {
+        tokio::select! {
+            Some(Ok(msg)) = msg_stream.next() => {
+                match msg {
+                    Message::Ping(bytes) => {
+                        if session.pong(&bytes).await.is_err() {
+                            break;
+                        }
                     }
+                    Message::Text(msg) => {
+                        let json_data: Result<MessageData,  serde_json::Error> = serde_json::from_str(&msg.to_string());
+                        println!("[WS] Got Message: {}", msg);
 
-                    let data = json_data.unwrap();
-                    let channel = data.channel.as_str();
-                    let data = data.data.as_document();
+                        if let Err(data) = json_data {
+                            beezle::print(format!("WS Error: {:?}", data).as_str());
+                            break;
+                        }
 
-                    match channel {
-                        /*
-                        DATA REQUIRED:
-                        handle: string;
-                        */
-                        "connect" => {
-                            if let Some(data) = data {
-                                user_handle = data.get("handle").expect("Handle not found").as_str().unwrap().to_string();
-                                let clone = Arc::clone(ws_sessions.get_ref());
-                                let locked = clone.lock();
+                        let data = json_data.unwrap();
+                        let channel = data.channel.as_str();
+                        let data = data.data.as_document();
 
-                                match locked {
-                                    Ok(mut sessions) => {
-                                        sessions.insert(user_handle.clone(), session.clone());
-                                        beezle::print("Inserted user to sessions.");
-                                    }
-                                    Err(err) => {
-                                        beezle::print(format!("Error in mutex: {:?}", err).as_str());
+                        match channel {
+                            "ping" => {
+                                last_heartbeat = Instant::now();
+                                let _ = session.text("{\"channel\": \"pong\", \"data\": {}}").await;
+                                // let locked = Arc::clone(ws_sessions.get_ref()).lock();
+                                    
+                                // match locked {
+                                //     Ok(mut sessions) => {
+                                //         let handle = data.unwrap().get("handle").unwrap().as_str().unwrap().to_string();
+                                //         beezle::print(format!("{} <-", handle).as_str());
+                                //         user_handle = handle.clone();
+                                //         (*sessions).insert(handle, session.clone());
+                                //         beezle::print("Inserted user to sessions.");
+                                //     }
+                                //     Err(err) => {
+                                //         beezle::print(format!("Error in mutex: {:?}", err).as_str());
+                                //     }
+                                // }
+                            }
+                            "pong" => {
+                                last_heartbeat = Instant::now();
+                            }
+                            /*
+                            DATA REQUIRED:
+                            handle: string;
+                            */
+                            "connect" => {
+                                if let Some(data) = data {
+                                    user_handle = data.get("handle").expect("Handle not found").as_str().unwrap().to_string();
+                                    let clone = Arc::clone(ws_sessions.get_ref());
+                                    let locked = clone.lock();
+                                    
+                                    match locked {
+                                        Ok(mut sessions) => {
+                                            sessions.insert(user_handle.clone(), session.clone());
+                                            beezle::print("Inserted user to sessions.");
+                                        }
+                                        Err(err) => {
+                                            beezle::print(format!("Error in mutex: {:?}", err).as_str());
+                                        }
                                     }
                                 }
                             }
-                        }
 
 
-                        /*
-                        DATA REQUIRED:
-                        talking_to: string;
-                        message: string;
-                        */
-                        "talk_other" => {
-                            if let Some(data) = data {
-                                let others_handle = data.get("talking_to").expect("talking_to not found").as_str().unwrap().to_string();
-                                let clone = Arc::clone(ws_sessions.get_ref());
-                                let locked = clone.lock();
-                                println!("Got message for channel talk_other {}", others_handle);
-
-                                match locked {
+                            /*
+                            DATA REQUIRED:
+                            talking_to: string;
+                            message: string;
+                            */
+                            "talk_other" => {
+                                if let Some(data) = data {
+                                    let others_handle = data.get("talking_to").expect("talking_to not found").as_str().unwrap().to_string();
+                                    let clone = Arc::clone(ws_sessions.get_ref());
+                                    let locked = clone.lock();
+                                    println!("Got message for channel talk_other {}", others_handle);
+                                    
+                                    match locked {
                                         Ok(mut sessions) => {
                                             for (handle, session) in sessions.iter() {
                                                 println!("Handle: '{}'", handle);
@@ -107,32 +145,70 @@ pub async fn main_ws(
                                     }
                                 }
                             }
-                        _ => {
+                            _ => {
                                 beezle::print("Channel not found");
                             }
                         }
-                }
-                Err(err) => {}
-            }
-        }
 
-        if !user_handle.is_empty() {
-            let clone = Arc::clone(ws_sessions.get_ref());
-            let locked = clone.lock();
-
-            match locked {
-                Ok(mut sessions) =>  {
-                    beezle::print("Session disconnected, removed user from session.");
-                    sessions.remove(&user_handle);
-                }
-                Err(err) => {
-                    beezle::print(format!("Error in mutex: {:?}", err).as_str());
+                    },
+                    _ => break,
                 }
             }
-        }
-        let _ = session.close(None).await;
-        beezle::print("Session closed");
-    });
+        
+            _ = interval.tick() => {
+                if Instant::now().duration_since(last_heartbeat) > CLIENT_TIMEOUT {
+                    break;
+                }
+                let send_data = doc! {
+                    "channel": "ping",
+                    "data": {}
+                };
+                let _ = session.text(serde_json::to_string(&send_data).unwrap()).await;
+            }
 
-    Ok(res)
+            else => {
+                break;
+            }
+        }
+    };
+
+    if !user_handle.is_empty() {
+        let clone = Arc::clone(ws_sessions.get_ref());
+        let locked = clone.lock();
+
+        match locked {
+            Ok(mut sessions) =>  {
+                beezle::print("Session disconnected, removed user from session.");
+                sessions.remove(&user_handle);
+            }
+            Err(err) => {
+                beezle::print(format!("Error in mutex: {:?}", err).as_str());
+            }
+        }
+    }
+    let _ = session.close(None).await;
+    beezle::print("Session closed");
+
+    // actix_web::rt::spawn(async move {
+    //     while let Some(Ok(msg)) = msg_stream.next().await {
+            
+    //     }
+
+    //     if !user_handle.is_empty() {
+    //         let locked = ws_sessions.lock();
+
+    //         match locked {
+    //             Ok(mut sessions) =>  {
+    //                 beezle::print("Session disconnected, removed user from session.");
+    //                 sessions.remove(&user_handle);
+    //             }
+    //             Err(err) => {
+    //                 beezle::print(format!("Error in mutex: {:?}", err).as_str());
+    //             }
+    //         }
+    //     }
+    //     let _ = session.close(None).await;
+    // });
+
+    // Ok(response)
 }
